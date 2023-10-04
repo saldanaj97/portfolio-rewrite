389607df2ead492b121d709de9f6ed459b218278
Juan Saldana <saldanaj97@gmail.com>
2023-10-04 13:30:45 -0500

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  arrowParens: 'always',
+  plugins: [require('prettier-plugin-tailwindcss')],
+  printWidth: 100,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none'
+};
